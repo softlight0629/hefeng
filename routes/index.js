dcaadc8228a03308b28278f36de31683538ed5d2
慧玄 <xuebin.gxb@alibaba-inc.com>
2017-07-29 01:19:43 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -9,34 +9,61 @@ var recommends = require('../lib/recommends');
 var video_db = require('../lib/video_db');
 var messages_db = require('../lib/messages_db');
 
+var side_categorys = products_db.get_side_categorys();
+var side_news = news_db.get_side_news();
+var all_products = products_db.get_all_products();
+
+function get_random_recommend() {
+  var a = [];
+  for (var  i = 0; i < recommends.length; i++) {
+    a[i] = i;
+  }
+
+  a.sort(function() {
+    return 0.5 - Math.random();
+  });
+
+  var recommends_list = [];
+  for (var i = 0; i < 4; i++) {
+    recommends_list[i] = recommends[a[i]];
+  }
+
+  return recommends_list;
+}
+
 router.get('/', function *(next) {
-  const news_list = news_db.slice(0, 9);
+  const news_list = side_news.slice(0, 9);
 
   yield this.render('index', {
     news_list,
     recommends,
+    side_categorys,
   });
 });
 
 
 router.get('/:path', function *(next) {
-  const news_list = news_db.slice(0, 9);
+  const news_list = side_news.slice(0, 9);
 
   yield this.render(this.params.path, {
-    news_list, 
+    news_list,
+    side_categorys,
   });
 });
 
 
 router.get('/products', function *(next) {
-  const news_list = news_db.slice(0, 9);
+  const news_list = side_news.slice(0, 9);
   const num = this.request.query.pageNum || 1;
   const pageNum = parseInt(num);
-  const paginator = pagination(products_db, pageNum, 12);
+  const paginator = pagination(all_products, pageNum, 12);
+  const recommends_list = get_random_recommend();
 
   yield this.render('products', {
     news_list, 
     paginator,
+    side_categorys,
+    recommends_list,
   });
 });
 
@@ -44,66 +71,75 @@ router.get('/products', function *(next) {
 router.get('/news', function *(next) {
   const num = this.request.query.pageNum || 1;
   const pageNum = parseInt(num);
-  const paginator = pagination(news_db, pageNum, 15);
+  const paginator = pagination(side_news, pageNum, 15);
+  const recommends_list = get_random_recommend();
 
   yield this.render('news', {
     paginator,
+    side_categorys,
+    recommends_list,
   });
 });
 
 router.get('/products/:cid', function* () {
-  const news_list = news_db.slice(0, 9);
+  const news_list = side_news.slice(0, 9);
   const num = this.request.query.pageNum || 1;
   const pageNum = parseInt(num);
+  const products = products_db.get_category_products(this.params.cid)
+  const recommends_list = get_random_recommend();
 
-  const paginator = pagination(products[this.params.cid], pageNum, 12);
+  const paginator = pagination(products, pageNum, 12);
 
   yield this.render('products', {
     cid: this.params.cid,
     paginator,
     news_list,
+    side_categorys,
+    recommends_list,
   });
 });
 
 
 router.get('/products/:cid/:id', function* () {
-  const product_list = products[this.params.cid];
-  const news_list = news_db.slice(0, 9);
-
-  let product;
-
-  for (const pd of product_list) {
-    if (pd.id == parseInt(this.params.id)) {
-      product = pd; 
-    }
-  }
+  const news_list = side_news.slice(0, 9);
+  const category = products_db.get_category(this.params.cid)
+  const product = category.products[this.params.id];
+  const prevnext = products_db.get_prev_next_products(this.params.id);
 
   yield this.render('product_show', {
     product, 
     news_list,
+    side_categorys,
+    prev: prevnext.prev,
+    next: prevnext.next,
   });
 });
 
 router.get('/news/:id', function* () {
-  const news_list = news_db.slice(0, 9);
-  console.log('asdasd');
-  const news = news_db[parseInt(this.params.id) - 1];
-  console.log(news);
+  const news_list = side_news.slice(0, 9);
+  const news = news_db.get_news(this.params.id);
+  const prevnext = news_db.get_prev_next_news(this.params.id);
+  const recommends_list = get_random_recommend();
 
   yield this.render('news_show', {
     news_list, 
     news,
+    side_categorys,
+    prev: prevnext.prev,
+    next: prevnext.next,
+    recommends_list,
   });
 });
 
 
 router.get('/videos/:id', function* () {
-  const news_list = news_db.slice(0, 9);
+  const news_list = side_news.slice(0, 9);
   const v = video_db[parseInt(this.params.id) - 1];
 
   yield this.render('video_show', {
     news_list, 
     v,
+    side_categorys,
   });
 });
 
